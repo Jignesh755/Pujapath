d62127e139467600f839c23d9deb56ad3ecbdbc6
Jignesh755 <jr08112005@gmail.com>
2025-05-02 15:37:09 +0530

    Add Home page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Welcome to PujaPath.com</h1>
+      <p>Your fresh puja flowers, delivered to your doorstep every morning.</p>
+    </div>
+  );
+}
